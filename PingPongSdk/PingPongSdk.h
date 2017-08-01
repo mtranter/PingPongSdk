9aef8445d394e813a0923069ba78cae8bbcb1134
mtranter <mark@marktranter.com>
2017-08-01 12:21:51 +0100

    Initial Commit

---
+++ PingPongSdk/PingPongSdk.h
@@ -0,0 +1,19 @@
+//
+//  PingPongSdk.h
+//  PingPongSdk
+//
+//  Created by Mark Tranter on 01/08/2017.
+//  Copyright © 2017 Mark Tranter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PingPongSdk.
+FOUNDATION_EXPORT double PingPongSdkVersionNumber;
+
+//! Project version string for PingPongSdk.
+FOUNDATION_EXPORT const unsigned char PingPongSdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PingPongSdk/PublicHeader.h>
+
+
