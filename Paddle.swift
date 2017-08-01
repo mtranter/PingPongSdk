efa0c4c0b8379b5623efcdacc98871b4212974ea
mtranter <mark@marktranter.com>
2017-08-01 13:51:51 +0100

    Move devicedna

--- Paddle.swift
+++ Paddle.swift
@@ -10,6 +10,11 @@ import Foundation
 import JudoKit
 
 public class Paddle {
+    
+    public init() {
+        
+    }
+    
     func doSomething() {
         let session = JudoKit(token: "bla", secret: "blubb")
         try? session.invokePayment("134134", amount: Amount(amountString: "34.12", currency: .GBP), reference: Reference(consumerRef: "some reference")!) { (response, error) in
